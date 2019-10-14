ab1f8feff6dc695b7cfed67a271b7be2f61511ab
kalogirou <vaskalogirou@gmail.com>
2019-10-14 15:26:24 +0300

    first commit

---
+++ src/main/java/com/kalogirou/test/GithubTestApplication.java
@@ -0,0 +1,11 @@
+package com.kalogirou.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTestApplication.class, args);
+	}
+}
